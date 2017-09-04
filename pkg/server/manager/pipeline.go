21f8a5c29895668610f28d32aa58a27646298808
Robin Yue <jmyue@hotmail.com>
2017-09-04 09:30:36 +0800

    Add pipeline rest APIs (#166)
    * add pipeline rest APIs
    
    * rename the function of deleting all pipelines in one project

--- pkg/server/manager/pipeline.go
+++ pkg/server/manager/pipeline.go
@@ -17,17 +17,113 @@ limitations under the License.
 package manager
 
 import (
+	"fmt"
+
+	"github.com/caicloud/cyclone/pkg/api"
 	"github.com/caicloud/cyclone/store"
 )
 
+// PipelineManager represents the interface to manage pipeline.
 type PipelineManager interface {
-	// CRUD
+	CreatePipeline(pipeline *api.Pipeline) (*api.Pipeline, error)
+	GetPipeline(projectName string, pipelineName string) (*api.Pipeline, error)
+	ListPipelines(projectName string) ([]api.Pipeline, error)
+	UpdatePipeline(projectName string, pipelineName string, newPipeline *api.Pipeline) (*api.Pipeline, error)
+	DeletePipeline(projectName string, pipelineName string) error
+	ClearPipelinesOfProject(projectName string) error
 }
 
+// pipelineManager represents the manager for pipeline.
 type pipelineManager struct {
+	dataStore *store.DataStore
 }
 
+// NewPipelineManager creates a pipeline manager.
 func NewPipelineManager(dataStore *store.DataStore) (PipelineManager, error) {
-	return nil, nil
+	if dataStore == nil {
+		return nil, fmt.Errorf("Fail to new pipeline manager as data store is nil.")
+	}
+
+	return &pipelineManager{dataStore}, nil
 }
 
+// CreatePipeline creates a pipeline.
+func (m *pipelineManager) CreatePipeline(pipeline *api.Pipeline) (*api.Pipeline, error) {
+	return m.dataStore.CreatePipeline(pipeline)
+}
+
+// GetPipeline gets the pipeline by name in one project.
+func (m *pipelineManager) GetPipeline(projectName string, pipelineName string) (*api.Pipeline, error) {
+	project, err := m.dataStore.FindProjectByName(projectName)
+	if err != nil {
+		return nil, err
+	}
+
+	return m.dataStore.FindPipelineByName(project.ID, pipelineName)
+}
+
+// ListPipelines lists all pipelines in one project.
+func (m *pipelineManager) ListPipelines(projectName string) ([]api.Pipeline, error) {
+	project, err := m.dataStore.FindProjectByName(projectName)
+	if err != nil {
+		return nil, err
+	}
+
+	return m.dataStore.FindPipelinesByProjectID(project.ID)
+}
+
+// UpdatePipeline updates the pipeline by name in one project.
+func (m *pipelineManager) UpdatePipeline(projectName string, pipelineName string, newPipeline *api.Pipeline) (*api.Pipeline, error) {
+	pipeline, err := m.GetPipeline(projectName, pipelineName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update the properties of the pipeline.
+	// TODO (robin) Whether need a method for this merge?
+	if len(newPipeline.Name) > 0 {
+		pipeline.Name = newPipeline.Name
+	}
+
+	if len(newPipeline.Description) > 0 {
+		pipeline.Description = newPipeline.Description
+	}
+
+	if len(newPipeline.Owner) > 0 {
+		pipeline.Owner = newPipeline.Owner
+	}
+
+	if newPipeline.Build != nil {
+		pipeline.Build = newPipeline.Build
+	}
+
+	if newPipeline.AutoTrigger != nil {
+		pipeline.AutoTrigger = newPipeline.AutoTrigger
+	}
+
+	if err = m.dataStore.UpdatePipeline(pipeline); err != nil {
+		return nil, err
+	}
+
+	return pipeline, nil
+}
+
+// DeletePipeline deletes the pipeline by name in one project.
+func (m *pipelineManager) DeletePipeline(projectName string, pipelineName string) error {
+	pipeline, err := m.GetPipeline(projectName, pipelineName)
+	if err != nil {
+		return err
+	}
+
+	return m.dataStore.DeletePipelineByID(pipeline.ID)
+}
+
+// ClearPipelinesOfProject deletes all pipelines in one project.
+func (m *pipelineManager) ClearPipelinesOfProject(projectName string) error {
+	project, err := m.dataStore.FindProjectByName(projectName)
+	if err != nil {
+		return err
+	}
+
+	return m.dataStore.DeletePipelinesByProjectID(project.ID)
+}
