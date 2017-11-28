3533cf64f874338ac754af14204525041b50e6d7
Robin Yue <jmyue@hotmail.com>
2017-11-28 14:47:50 +0800

    refact(stability): retry to connect MongoDB (#263)

--- store/store.go
+++ store/store.go
@@ -17,7 +17,11 @@ limitations under the License.
 package store
 
 import (
+	"time"
+
+	log "github.com/golang/glog"
 	"gopkg.in/mgo.v2"
+	"k8s.io/client-go/pkg/util/wait"
 )
 
 const (
@@ -37,10 +41,15 @@ const (
 	projectCollectionName        string = "projects"
 	pipelineCollectionName       string = "pipelines"
 	pipelineRecordCollectionName string = "pipelineRecords"
+
+	socketTimeout  = time.Second * 5
+	syncTimeout    = time.Second * 5
+	tickerDuration = time.Second * 5
 )
 
 var (
 	session *mgo.Session
+	mclosed chan struct{}
 )
 
 // DataStore is the type for mongo db store.
@@ -54,18 +63,41 @@ type DataStore struct {
 }
 
 // Init store mongo client session
-func Init(s *mgo.Session) {
-	session = s
+func Init(host string, gracePeriod time.Duration, closing chan struct{}) (*mgo.Session, chan struct{}, error) {
+	mclosed = make(chan struct{})
+	var err error
+
+	// dail mongo session
+	// wait mongodb set up
+	wait.Poll(time.Second, gracePeriod, func() (bool, error) {
+		session, err = mgo.Dial(host)
+		return err == nil, nil
+	})
+
+	if err != nil {
+		log.Errorf("Unable connect to mongodb addr %s", host)
+		return nil, nil, err
+	}
+
+	log.Infof("connect to mongodb addr: %s", host)
+	session.SetMode(mgo.Strong, true)
+
+	go backgroundMongo(closing)
+
+	return session, mclosed, nil
 }
 
 // NewStore copy a mongo client session
 func NewStore() *DataStore {
 	s := session.Copy()
+
+	// s is for old api, it will be closed after each use.
+	// The new collections are for new api, they will be reused.
 	return &DataStore{
 		s:                        s,
-		projectCollection:        s.DB(defaultDBName).C(projectCollectionName),
-		pipelineCollection:       s.DB(defaultDBName).C(pipelineCollectionName),
-		pipelineRecordCollection: s.DB(defaultDBName).C(pipelineRecordCollectionName),
+		projectCollection:        session.DB(defaultDBName).C(projectCollectionName),
+		pipelineCollection:       session.DB(defaultDBName).C(pipelineCollectionName),
+		pipelineRecordCollection: session.DB(defaultDBName).C(pipelineRecordCollectionName),
 	}
 }
 
@@ -77,4 +109,25 @@ func (d *DataStore) Close() {
 // Ping ping mongo server
 func (d *DataStore) Ping() error {
 	return d.s.Ping()
+}
+
+// Background goroutine for mongo. It can hold mongo connection & close session when progress exit.
+func backgroundMongo(closing chan struct{}) {
+	ticker := time.NewTicker(tickerDuration)
+	for {
+		select {
+		case <-ticker.C:
+			if err := session.Ping(); err != nil {
+				log.Errorf("Ping Mongodb with error %s", err.Error())
+				session.Refresh()
+				session.SetSocketTimeout(socketTimeout)
+				session.SetSyncTimeout(syncTimeout)
+			}
+		case <-closing:
+			session.Close()
+			log.Info("Mongodb session has been closed")
+			close(mclosed)
+			return
+		}
+	}
 }
