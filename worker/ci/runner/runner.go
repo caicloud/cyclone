49f73b94ab7a63ff6aea8f93c6c0b3bdb59093d4
Jinming Yue <jmyue@hotmail.com>
2018-02-28 19:27:42 +0800

    refact(clair): cleanup the feature of image security (#322) (#360)
    Automatic merge from submit-queue.
    
    refact(clair): cleanup the feature of image security (#322)
    
    **What this PR does / why we need it**:
    
    Cherry pick #322 into release-3.0 branch.
    
    **Which issue(s) this PR fixes** *(optional, close the issue(s) when PR gets merged)*:
    
    Fixes #203
    
    **Special notes for your reviewer**:
    
    /cc @bbbmj 
    
    **Release note**:
    
    ```release-note
    Cleanup the feature of image security
    ```

--- worker/ci/runner/runner.go
+++ worker/ci/runner/runner.go
@@ -21,12 +21,9 @@ import (
 	"fmt"
 
 	"github.com/caicloud/cyclone/api"
-	"github.com/caicloud/cyclone/cloud"
 	"github.com/caicloud/cyclone/docker"
 	"github.com/caicloud/cyclone/pkg/log"
-	"github.com/caicloud/cyclone/pkg/osutil"
 	"github.com/caicloud/cyclone/worker/ci/parser"
-	"github.com/caicloud/cyclone/worker/clair"
 	steplog "github.com/caicloud/cyclone/worker/log"
 )
 
@@ -113,7 +110,7 @@ func (b *Build) walk(node parser.Node) (err error) {
 			createContainerOptions := toBuildContainerConfig(node, b, parser.NodeIntegration)
 
 			// The tag of integration image should be appended.
-			if tagname, ok := b.event.Data["tag-name"];ok {
+			if tagname, ok := b.event.Data["tag-name"]; ok {
 				createContainerOptions.Config.Image = createContainerOptions.Config.Image + ":" + tagname.(string)
 			}
 
@@ -208,11 +205,6 @@ func (b *Build) PublishImage() (err error) {
 
 	// Now image is pushed to registry successfully.
 	b.status |= pushImageSuccess
-
-	disableClair := osutil.GetBoolEnv(cloud.ClairDisable, false)
-	if !disableClair {
-		clair.Analysis(b.event, b.dockerManager)
-	}
 	steplog.InsertStepLog(b.event, steplog.PushImage, steplog.Finish, nil)
 	return nil
 }
