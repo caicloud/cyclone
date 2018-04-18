c6ec431a8c5b93061cb1fef0cdd478e75bcbb935
zhujian7 <zhujian@caicloud.io>
2018-04-18 17:06:24 +0800

    feat(worker):update the docker in worker to docker:1.13.1-dind

--- pkg/docker/client.go
+++ pkg/docker/client.go
@@ -150,7 +150,7 @@ func (dm *DockerManager) StartContainer(options docker_client.CreateContainerOpt
 	}
 
 	// Run the container
-	err = dm.Client.StartContainer(container.ID, options.HostConfig)
+	err = dm.Client.StartContainer(container.ID, nil)
 	if err != nil {
 		return "", fmt.Errorf("start container with error %s", err.Error())
 	}
