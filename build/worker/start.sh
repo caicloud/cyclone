0a25146f61de64999cc7a7a6e197164b7fc861ec
Jian Zhu <36154065+zhujian7@users.noreply.github.com>
2018-07-16 11:15:52 +0800

    feat(worker):update worker storage driver from vfs to overlay (#536)

--- build/worker/start.sh
+++ build/worker/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-sed -i '/2375/d' /usr/local/bin/dockerd-entrypoint.sh
-dockerd-entrypoint.sh > /dev/null &
+dockerd-entrypoint.sh dockerd > /dev/null &
 sleep 10 
 /cyclone-worker
