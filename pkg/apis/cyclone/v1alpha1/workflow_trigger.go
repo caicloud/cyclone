6feeaa676f65e4acde257865e03216f45e2fb1be
De Chen <chende@caicloud.io>
2018-11-13 09:23:46 +0800

    fix: crd definition problems (#598)

--- pkg/apis/cyclone/v1alpha1/workflow_trigger.go
+++ pkg/apis/cyclone/v1alpha1/workflow_trigger.go
@@ -1,6 +1,9 @@
 package v1alpha1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -27,10 +30,12 @@ const (
 
 // WorkflowTriggerSpec defines workflow trigger definition.
 type WorkflowTriggerSpec struct {
-	// Name of the workflow to trigger
-	WorkflowRef string `json:"workflowRef"`
+	// Reference of the Workflow to trigger
+	WorkflowRef *corev1.ObjectReference `json:"workflowRef"`
 	// Type of this trigger, Schedule or Webhook
 	Type TriggerType `json:"triggerType"`
+	// Whether this trigger is enabled, if set to true, no workflow will be triggered
+	Enabled bool `json:"enabled"`
 	// Parameters of the trigger, for Schedule type trigger, "schedule"
 	// parameter is required
 	Parameters []ParameterItem `json:"parameters"`
@@ -40,4 +45,13 @@ type WorkflowTriggerSpec struct {
 type WorkflowTriggerStatus struct {
 	// How many times this trigger got triggered
 	Count int `json:"count"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// WorkflowTriggerList describes an array of WorkflowTrigger instances.
+type WorkflowTriggerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []WorkflowTrigger `json:"items""`
 }
