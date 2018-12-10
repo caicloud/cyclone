e7d5995c6f750a16d33adacc40670df67386e083
zhujian7 <zhujian@caicloud.io>
2018-12-10 22:50:13 +0800

    test: add tests

--- pkg/util/os/os_test.go
+++ pkg/util/os/os_test.go
@@ -17,6 +17,7 @@ package os
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -62,4 +63,63 @@ func TestMustGet(t *testing.T) {
 	f = GetFloat64Env(float_env, 0)
 	assert.Equal(t, f, 11.1)
 
+}
+
+func Test_IsFileExists(t *testing.T) {
+	home := GetHomeDir()
+	existFile := filepath.Join(home, "cyclone.112233445566")
+	os.Create(existFile)
+	defer os.RemoveAll(existFile)
+
+	testCases := map[string]struct {
+		file string
+		flag bool
+	}{
+		"exist": {
+			existFile,
+			true,
+		},
+		"not exist": {
+			"cyclone.11223344556677",
+			false,
+		},
+	}
+
+	for d, tc := range testCases {
+		flag := IsFileExists(tc.file)
+		if flag != tc.flag {
+			t.Errorf("%s failed as error Expect flag %v to be %v", d, flag, tc.flag)
+		}
+	}
+}
+
+func Test_GetBoolEnv(t *testing.T) {
+	testEnv := "cyclone.112233445566"
+	testValue := "true"
+	os.Setenv(testEnv, testValue)
+	defer os.Unsetenv(testEnv)
+
+	testCases := map[string]struct {
+		env          string
+		defaultValue bool
+		value        bool
+	}{
+		"not default": {
+			testEnv,
+			false,
+			true,
+		},
+		"default": {
+			"cyclone.11223344556677",
+			true,
+			true,
+		},
+	}
+
+	for d, tc := range testCases {
+		value := GetBoolEnv(tc.env, tc.defaultValue)
+		if value != tc.value {
+			t.Errorf("%s failed as error Expect value %v to be %v", d, value, tc.value)
+		}
+	}
 }
