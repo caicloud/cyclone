f3ecbcd09bc480cf15bea744c9cc016058e7872b
cd1989 <chende@caicloud.io>
2019-01-31 15:04:48 +0800

    fix: remove resource route

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -2,7 +2,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Project from '../components/project';
 import Workflow from '../components/workflow';
 import StageTemplate from '../components/stageTemplate';
-import Resource from '../components/resource';
 import Overview from '../components/overview';
 import Integration from '../components/integration';
 import Swagger from '../components/swagger';
@@ -12,7 +11,6 @@ const Routes = () => (
   <Switch>
     <Route path="/" exact component={Overview} />
     <Route path="/project" component={Project} />
-    <Route path="/resource" component={Resource} />
     <Route path="/stageTemplate" component={StageTemplate} />
     <Route path="/workflow" component={Workflow} />
     <Route path="/integration" component={Integration} />
