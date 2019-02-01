9d165d6ae9a270d6f67f47defc47473355508928
caoshanmin <caoshanmin@caicloud.io>
2019-02-01 18:07:51 +0800

    feat(template): add Detail-Page

---
+++ web/src/components/stageTemplate/Detail/Configuration.js
@@ -0,0 +1,70 @@
+import KeyValue from '@/components/public/KeyValue';
+import { Table } from 'antd';
+
+import style from './detail.module.less';
+
+const envColumns = [
+  {
+    title: intl.get('stage.spec.container.env.name'),
+    dataIndex: 'name',
+    width: '160px',
+  },
+  {
+    title: intl.get('stage.spec.container.env.value'),
+    dataIndex: 'value',
+    width: '160px',
+  },
+];
+
+const Configuration = ({ configuration = {} }) => {
+  const containers = _.get(configuration, 'containers', []);
+  return _.map(containers, (container, index) => (
+    <div key={container.name || index}>
+      <KeyValue
+        tip="ss"
+        cls={style['kv-item']}
+        name={intl.get('stage.spec.container.image')}
+        value={container.image}
+      />
+      <KeyValue
+        cls={style['kv-item']}
+        name={'EntryPoint'}
+        isEmpty={_.isEmpty(container.command)}
+        value={
+          <div>
+            {_.map(container.command, cmd => (
+              <div key={cmd}>{cmd}</div>
+            ))}
+          </div>
+        }
+      />
+      <KeyValue
+        cls={style['kv-item']}
+        name={'CMD'}
+        isEmpty={_.isEmpty(container.args)}
+        value={
+          <div>
+            {_.map(container.args, arg => (
+              <div key={arg}>{arg}</div>
+            ))}
+          </div>
+        }
+      />
+      <KeyValue
+        cls={style['kv-item']}
+        name={intl.get('stage.spec.container.envs')}
+        isEmpty={_.isEmpty(container.env)}
+        value={
+          <Table
+            columns={envColumns}
+            dataSource={container.env}
+            size="small"
+            pagination={false}
+          />
+        }
+      />
+    </div>
+  ));
+};
+
+export default Configuration;
---
+++ web/src/components/stageTemplate/Detail/Inputs.js
@@ -0,0 +1,67 @@
+import { Table, Collapse } from 'antd';
+import PropTypes from 'prop-types';
+
+const Inputs = ({ inputs = {} }) => {
+  const resourceColumns = [
+    {
+      title: intl.get('name'),
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: intl.get('path'),
+      dataIndex: 'path',
+    },
+  ];
+
+  const argumentColumns = [
+    {
+      title: intl.get('name'),
+      dataIndex: 'name',
+      key: 'name',
+    },
+  ];
+
+  return (
+    <Collapse activeKey={['1', '2']}>
+      <Collapse.Panel showArrow={false} header={intl.get('resources')} key="1">
+        <Table
+          columns={resourceColumns}
+          dataSource={inputs.resources}
+          pagination={false}
+          rowKey="name"
+        />
+      </Collapse.Panel>
+      <Collapse.Panel
+        showArrow={false}
+        header={intl.get('stage.input.arguments')}
+        key="2"
+      >
+        <Table
+          columns={argumentColumns}
+          dataSource={inputs.arguments}
+          pagination={false}
+          rowKey="name"
+        />
+      </Collapse.Panel>
+    </Collapse>
+  );
+};
+
+Inputs.propTypes = {
+  inputs: PropTypes.shape({
+    resources: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        path: PropTypes.string,
+      })
+    ).isRequired,
+    arguments: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+      })
+    ).isRequired,
+  }),
+};
+
+export default Inputs;
