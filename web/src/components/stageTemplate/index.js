eeb2700d4d899fda9f15e5f740c590103e141796
caoshanmin <caoshanmin@caicloud.io>
2019-02-01 18:08:34 +0800

    feat(template): add sub-route for detail

--- web/src/components/stageTemplate/index.js
+++ web/src/components/stageTemplate/index.js
@@ -1,2 +1,18 @@
 import List from './List/index';
-export default List;
+import Detail from './Detail/index';
+import { Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const TemplateRoute = ({ match }) => {
+  return (
+    <Switch>
+      <Route path="/stageTemplate" exact component={List} />
+      <Route path={`${match.path}/:templateName`} component={Detail} />
+    </Switch>
+  );
+};
+
+TemplateRoute.propTypes = {
+  match: PropTypes.object,
+};
+export default TemplateRoute;
