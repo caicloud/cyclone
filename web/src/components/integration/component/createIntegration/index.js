ee1e2323fb3a3c0db85313d8a5f30d50a484e4da
Xiangtao Hu <huxiangtao@caicloud.io>
2019-02-01 15:23:17 +0800

    feat(integrationCenter): add text switch

--- web/src/components/integration/component/createIntegration/index.js
+++ web/src/components/integration/component/createIntegration/index.js
@@ -1,11 +1,21 @@
 import IntegrationForm from './FormWrap';
 import { Row, Col } from 'antd';
+import { PropTypes } from 'mobx-react';
 
 const CreateIntegration = props => {
+  const {
+    match: { params },
+  } = props;
+  const update = !!_.get(params, 'integrationName');
+
   return (
     <div className="integration-form">
       <div className="head-bar">
-        <h2>{intl.get('integration.addexternalsystem')}</h2>
+        <h2>
+          {update
+            ? intl.get('integration.updateexternalsystem')
+            : intl.get('integration.addexternalsystem')}
+        </h2>
       </div>
       <Row>
         <Col span={20}>
@@ -14,6 +24,10 @@ const CreateIntegration = props => {
       </Row>
     </div>
   );
+};
+
+CreateIntegration.propTypes = {
+  match: PropTypes.object,
 };
 
 export default CreateIntegration;
