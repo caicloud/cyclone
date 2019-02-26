63b2622c84662b7233ed870d7bfe347517664af3
C0reFast <topchenfu@qq.com>
2019-02-26 14:15:11 +0800

    add arg -w0 to base64, fix make deploy error

--- manifests/generate.sh
+++ manifests/generate.sh
@@ -52,7 +52,7 @@ DOCKER_CONFIG=$(cat <<-END
 {
   "auths": {
     "${REGISTRY%/*}": {
-        "auth": "$(echo -n $AUTH | base64)"
+        "auth": "$(echo -n $AUTH | base64 -w0)"
     }
   }
 }
@@ -64,7 +64,7 @@ if [ ! -e ./.generated ]; then
     mkdir ./.generated
 fi
 sed -e "s/__REGISTRY__/${REGISTRY/\//\\/}/g" \
-    -e "s/__REGISTRY_AUTH__/$(echo $DOCKER_CONFIG | base64)/g" \
+    -e "s/__REGISTRY_AUTH__/$(echo $DOCKER_CONFIG | base64 -w0)/g" \
     -e "s/__PVC__/${PVC}/g" \
     -e "s/__VERSION__/${VERSION}/g" \
     ./cyclone.yaml.template > ./.generated/cyclone.yaml
