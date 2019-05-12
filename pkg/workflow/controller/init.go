1d983cb3321b93f72fd282410bef1ebdd9b673cb
De Chen <chende@caicloud.io>
2019-05-12 18:11:26 +0800

    Create default ExecutionCluster CR for control cluster (#980)
    * fix: default cluster when empty in execution context
    
    * fix: create default ExecutionCluster for control cluster in engine

--- pkg/workflow/controller/init.go
+++ pkg/workflow/controller/init.go
@@ -5,6 +5,12 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/api/errors"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/caicloud/cyclone/pkg/apis/cyclone/v1alpha1"
+	"github.com/caicloud/cyclone/pkg/common"
+	"github.com/caicloud/cyclone/pkg/k8s/clientset"
 )
 
 // InitLogger inits logging
@@ -30,4 +36,24 @@ func InitLogger(logging *LoggingConfig) {
 		FullTimestamp: true,
 	})
 	log.SetOutput(os.Stdout)
+}
+
+// InitControlCluster initializes control cluster for workflow to run.
+func InitControlCluster(client clientset.Interface) error {
+	// Create ExecutionCluster instance for control cluster. This makes it possible to
+	// use only workflow engine to run workflow in control cluster.
+	// Create ExecutionCluster resource for Workflow Engine to use
+	_, err := client.CycloneV1alpha1().ExecutionClusters().Create(&v1alpha1.ExecutionCluster{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name: common.ControlClusterName,
+		},
+	})
+
+	// If the CR already exists, just ignore it.
+	if err != nil && errors.IsAlreadyExists(err) {
+		log.Infof("ExecutionCluster resource '%s' already exist", common.ControlClusterName)
+		return nil
+	}
+
+	return err
 }
