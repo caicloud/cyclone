ce3f37bb65e5a40145b31fa1853ed48938774ce3
cd1989 <chende@caicloud.io>
2019-06-02 18:06:05 +0800

    feat: add status filter to wfr list

--- web/src/components/workflow/component/detail/WorkflowRuns.js
+++ web/src/components/workflow/component/detail/WorkflowRuns.js
@@ -1,16 +1,22 @@
 import EllipsisMenu from '@/components/public/ellipsisMenu';
 import { inject, observer } from 'mobx-react';
-import { Table, Modal, Tag, Spin, Input } from 'antd';
+import { Table, Modal, Tag, Spin, Input, Select } from 'antd';
 import { FormatTime, TimeDuration } from '@/lib/util';
 import PropTypes from 'prop-types';
 
 const confirm = Modal.confirm;
 const Fragment = React.Fragment;
 const Search = Input.Search;
+const { Option } = Select;
 
 @inject('workflow')
 @observer
 class WorkflowRuns extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { status: 'all', query: '' };
+  }
+
   static propTypes = {
     workflow: PropTypes.shape({
       delelteWorkflowRun: PropTypes.func,
@@ -49,17 +55,35 @@ class WorkflowRuns extends React.Component {
     });
   };
 
-  search = val => {
+  doSearch = () => {
+    const { query, status } = this.state;
     const {
       workflow: { listWorkflowRuns },
       projectName,
       workflowName,
     } = this.props;
-    listWorkflowRuns(projectName, workflowName, {
-      filter: `name=${val}`,
-      sort: true,
-      ascending: false,
-    });
+
+    if (status === 'all') {
+      listWorkflowRuns(projectName, workflowName, {
+        filter: `name=${query}`,
+        sort: true,
+        ascending: false,
+      });
+    } else {
+      listWorkflowRuns(projectName, workflowName, {
+        filter: `name=${query},status=${status}`,
+        sort: true,
+        ascending: false,
+      });
+    }
+  };
+
+  onStatusSelectChange = v => {
+    this.setState({ status: v }, this.doSearch);
+  };
+
+  search = val => {
+    this.setState({ query: val }, this.doSearch);
   };
 
   render() {
@@ -68,11 +92,15 @@ class WorkflowRuns extends React.Component {
       projectName,
       workflowName,
     } = this.props;
+
+    const { status } = this.state;
+
     const items = _.get(
       workflowRuns,
       [`${projectName}-${workflowName}`, 'items'],
       []
     );
+
     const columns = [
       {
         title: intl.get('name'),
@@ -146,8 +174,19 @@ class WorkflowRuns extends React.Component {
     return (
       <Fragment>
         <div className="head-bar right">
+          <Select
+            defaultValue="all"
+            value={status}
+            style={{ width: 120, marginRight: 16 }}
+            onChange={this.onStatusSelectChange}
+          >
+            <Option value="all">{intl.get('status.all')}</Option>
+            <Option value="succeeded">{intl.get('status.succeeded')}</Option>
+            <Option value="running">{intl.get('status.running')}</Option>
+            <Option value="failed">{intl.get('status.failed')}</Option>
+          </Select>
           <Search
-            placeholder="input search text"
+            placeholder="input record name query"
             onSearch={this.search}
             style={{ width: 200 }}
           />
