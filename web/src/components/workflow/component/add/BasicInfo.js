07788a99dd46122ddc00c4a727e21a22baa53bd8
Xiaolian Qiu <qme@caicloud.io>
2019-06-03 17:56:58 +0800

    fix(workflow): workflow name read only when update (#1108)

--- web/src/components/workflow/component/add/BasicInfo.js
+++ web/src/components/workflow/component/add/BasicInfo.js
@@ -1,24 +1,38 @@
 import { Input, Form } from 'antd';
-import MakeField from '@/components/public/makeField';
 import { Field } from 'formik';
+import PropTypes from 'prop-types';
+import { defaultFormItemLayout } from '@/lib/const';
 import { required } from '@/components/public/validate';
+import MakeField from '@/components/public/makeField';
 
+const FormItem = Form.Item;
 const { TextArea } = Input;
 const InputField = MakeField(Input);
 const TextareaField = MakeField(TextArea);
 
 class BasicInfo extends React.Component {
+  static propTypes = {
+    update: PropTypes.bool,
+    values: PropTypes.object,
+  };
   render() {
+    const { update, values } = this.props;
     return (
       <Form>
-        <Field
-          label={intl.get('name')}
-          name="metadata.name"
-          component={InputField}
-          hasFeedback
-          required
-          validate={required}
-        />
+        {update ? (
+          <FormItem label={intl.get('name')} {...defaultFormItemLayout}>
+            {_.get(values, `metadata.name`)}
+          </FormItem>
+        ) : (
+          <Field
+            label={intl.get('name')}
+            name="metadata.name"
+            component={update ? _.get(values, `metadata.name`) : InputField}
+            hasFeedback
+            required
+            validate={required}
+          />
+        )}
         <Field
           label={intl.get('description')}
           name="metadata.annotations.description"
