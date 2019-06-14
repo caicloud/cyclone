541958eaa5eea2767d98c5e1de9a0f66b3128285
Jinming Yue <yuejinming@caicloud.io>
2019-06-14 15:08:55 +0800

    chore(vendor): update k8s to 1.14.2 (#1139)
    * chore(vendor): update k8s to 1.14.2
    
    * fix dep version
    
    * fix client-gen
    
    * update generate code

--- pkg/k8s/clientset/typed/cyclone/v1alpha1/fake/fake_stage.go
+++ pkg/k8s/clientset/typed/cyclone/v1alpha1/fake/fake_stage.go
@@ -107,7 +107,7 @@ func (c *FakeStages) DeleteCollection(options *v1.DeleteOptions, listOptions v1.
 // Patch applies the patch and returns the patched stage.
 func (c *FakeStages) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.Stage, err error) {
 	obj, err := c.Fake.
-		Invokes(testing.NewPatchSubresourceAction(stagesResource, c.ns, name, data, subresources...), &v1alpha1.Stage{})
+		Invokes(testing.NewPatchSubresourceAction(stagesResource, c.ns, name, pt, data, subresources...), &v1alpha1.Stage{})
 
 	if obj == nil {
 		return nil, err
