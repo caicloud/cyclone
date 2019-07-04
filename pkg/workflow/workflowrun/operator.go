8f807e246b07727173d65fdad5ce1e4ccc954e59
zhujian <zhujian@caicloud.io>
2019-07-04 12:00:13 +0800

    fix: update wfr with global variable resolved

--- pkg/workflow/workflowrun/operator.go
+++ pkg/workflow/workflowrun/operator.go
@@ -546,7 +546,6 @@ func (o *operator) ResolveGlobalVariables() {
 	}
 
 	var appendVariables []v1alpha1.GlobalVariable
-
 	for _, wfVariable := range o.wf.Spec.GlobalVariables {
 		var found bool
 		for _, variable := range o.wfr.Spec.GlobalVariables {
@@ -562,8 +561,29 @@ func (o *operator) ResolveGlobalVariables() {
 				Value: values.GenerateValue(wfVariable.Value),
 			})
 		}
-
 	}
 
 	o.wfr.Spec.GlobalVariables = append(o.wfr.Spec.GlobalVariables, appendVariables...)
+
+	// Update WorkflowRun's GlobalVariables
+	if len(appendVariables) > 0 {
+		log.WithField("variables", appendVariables).Info("Append variables from wf to wfr")
+		latest, err := o.client.CycloneV1alpha1().WorkflowRuns(o.wfr.Namespace).Get(o.wfr.Name, metav1.GetOptions{})
+		if err != nil {
+			log.WithField("wfr", latest.Name).
+				WithField("error", err).
+				Warn("WorkflowRun global variables updating, get latest WorkflowRun failed.")
+			return
+		}
+
+		latest.Spec.GlobalVariables = o.wfr.Spec.GlobalVariables
+		_, err = o.client.CycloneV1alpha1().WorkflowRuns(latest.Namespace).Update(latest)
+		if err != nil {
+			log.WithField("wfr", latest.Name).
+				WithField("error", err).
+				Warn("WorkflowRun global variables updated failed.")
+			return
+		}
+	}
+
 }
