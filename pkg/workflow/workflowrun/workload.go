1cfd56f9231812a204f1c7d698c3bf847dc01603
Jian Zeng <zengjian@caicloud.io>
2020-11-27 14:34:14 +0800

    feat(wfr): retry creating pod on QuotaExceed error until timeout (#1528)
    * feat(wfr): retry creating pod on QuotaExceed error until timeout
    
    * chore(controller): adjust log level
    
    * fix(wfr): correcly check error

--- pkg/workflow/workflowrun/workload.go
+++ pkg/workflow/workflowrun/workload.go
@@ -7,13 +7,11 @@ import (
 	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/caicloud/cyclone/pkg/apis/cyclone/v1alpha1"
 	"github.com/caicloud/cyclone/pkg/k8s/clientset"
 	"github.com/caicloud/cyclone/pkg/util"
-	"github.com/caicloud/cyclone/pkg/util/retry"
 	"github.com/caicloud/cyclone/pkg/workflow/workload/delegation"
 	"github.com/caicloud/cyclone/pkg/workflow/workload/pod"
 )
@@ -68,7 +66,6 @@ func (p *WorkloadProcessor) processPod() error {
 	// Generate pod for this stage.
 	po, err := pod.NewBuilder(p.client, p.wf, p.wfr, p.stg).Build()
 	if err != nil {
-		log.WithField("wfr", p.wfr.Name).WithField("stg", p.stg.Name).Error("Create pod manifest for stage error: ", err)
 		p.wfrOper.GetRecorder().Eventf(p.wfr, corev1.EventTypeWarning, "GeneratePodSpecError", "Generate pod for stage '%s' error: %v", p.stg.Name, err)
 		p.wfrOper.UpdateStageStatus(p.stg.Name, &v1alpha1.Status{
 			Phase:              v1alpha1.StatusFailed,
@@ -76,35 +73,26 @@ func (p *WorkloadProcessor) processPod() error {
 			LastTransitionTime: metav1.Time{Time: time.Now()},
 			Message:            fmt.Sprintf("Failed to generate pod: %v", err),
 		})
-		return err
+		return fmt.Errorf("create pod manifest: %w", err)
 	}
 	log.WithField("stg", p.stg.Name).Debug("Pod manifest created")
 
-	// Create the generated pod with retry on exceeded quota.
-	// Here is a litter tricky. Cyclone will delete stage related pod to release cpu/memory resource when stage have
-	// been finished, but pod deletion needs some time, so retry on exceeded quota gives the time to waiting previous
-	// stage pod deletion.
-	backoff := wait.Backoff{
-		Steps:    3,
-		Duration: 5 * time.Second,
-		Factor:   1.5,
-		Jitter:   0.1,
-	}
-	origin := po.DeepCopy()
-	err = retry.OnExceededQuota(backoff, func() error {
-		po, err = p.clusterClient.CoreV1().Pods(pod.GetExecutionContext(p.wfr).Namespace).Create(origin)
-		return err
-	})
+	po, err = p.clusterClient.CoreV1().Pods(pod.GetExecutionContext(p.wfr).Namespace).Create(po)
 	if err != nil {
-		log.WithField("wfr", p.wfr.Name).WithField("stg", p.stg.Name).Error("Create pod for stage error: ", err)
 		p.wfrOper.GetRecorder().Eventf(p.wfr, corev1.EventTypeWarning, "StagePodCreated", "Create pod for stage '%s' error: %v", p.stg.Name, err)
+		var phase v1alpha1.StatusPhase
+		if isExceededQuotaError(err) {
+			phase = v1alpha1.StatusPending
+		} else {
+			phase = v1alpha1.StatusFailed
+		}
 		p.wfrOper.UpdateStageStatus(p.stg.Name, &v1alpha1.Status{
-			Phase:              v1alpha1.StatusFailed,
+			Phase:              phase,
 			Reason:             "CreatePodError",
 			LastTransitionTime: metav1.Time{Time: time.Now()},
 			Message:            fmt.Sprintf("Failed to create pod: %v", err),
 		})
-		return err
+		return fmt.Errorf("create pod: %w", err)
 	}
 
 	log.WithField("wfr", p.wfr.Name).WithField("stg", p.stg.Name).Debug("Create pod for stage succeeded")
